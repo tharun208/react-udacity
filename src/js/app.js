22bf308c95ef472c7afc6a327a086d433c7b87b7
jeeva <jeeva@tricog.com>
2018-04-06 20:21:51 +0530

    follow-this-branch

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import '../css/style.css';
+export default class Hello extends Component {
+  render() {
+    return (
+      <div>
+        Hello from react
+      </div>
+    );
+  }
+}
+
+render(<Hello />, document.getElementById('app'));
