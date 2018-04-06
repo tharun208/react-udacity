3058fbd25dd675e21351592f319cdefea2a36eb2
tharun208 <rajendrantharun@live.com>
2018-04-07 01:50:39 +0530

    Added all the functionalities and generated cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,61 @@
 import React, { Component } from 'react';
 import './App.css';
-import Data  from './JsonData/document.json';
-import Cards from './Cards'
-var cardData = Data;
+import Cards from './Cards';
+import Data from './JsonData/document.json'
 class App extends Component {
+  constructor()
+  {   super();
+      this.state={course:'popular'};
+  }
   showCourses(courseName) {
-    console.log('courseName------->',courseName);
+    this.setState({course:courseName});
+  }
+  renderCards(courseName) {
+    console.log(courseName);
+    if(courseName === 'popular'){
+    return Data.popular.map((i) => {
+      return <div><Cards name={i.Name} instructor={i.Instructor}/></div>
+    })
+  }
+  else if(courseName === 'Machine Learning and Deep Learning'){
+    return Data.msd.map((i) => {
+      return <div><Cards name={i.Name} instructor={i.Instructor}/></div>
+    })
+  }
+    else if(courseName === 'Data Science'){
+      return Data.ds.map((i) => {
+        return <div><Cards name={i.Name} instructor={i.Instructor}/></div>
+      })
+    }
+    else if(courseName === 'Artificial Intelligence'){
+      return Data.ai.map((i) => {
+        return <div><Cards name={i.Name} instructor={i.Instructor}/></div>
+      })
+    }
+    else if(courseName === 'Mobile Development'){
+      return Data.md.map((i) => {
+        return <div><Cards name={i.Name} instructor={i.Instructor}/></div>
+      })
+    }
+    else if(courseName === 'Web Development'){
+      return Data.wd.map((i) => {
+        return <div><Cards name={i.Name} instructor={i.Instructor}/></div>
+      })
+    }
+    else if(courseName === 'Virtual Reality'){
+      return Data.vr.map((i) => {
+        return <div><Cards name={i.Name} instructor={i.Instructor}/></div>
+      })
+    }
+    else if(courseName === 'Digital Marketing'){
+      return Data.dm.map((i) => {
+        return <div><Cards name={i.Name} instructor={i.Instructor}/></div>
+      })
+    } else {
+    return Data.as.map((i) => {
+      return <div><Cards name={i.Name} instructor={i.Instructor}/></div>
+    })
+  }
   }
   renderButtons(btnarray) {
     return btnarray.map((i,index) =>{
@@ -13,12 +63,15 @@ class App extends Component {
   })
   }
   render() {
-    const btnarray=['Popular','Machine Learning and Deep Learning','Data Science','Artificial Intelligence','Mobile Development','Web Development','Virtual Reality','Digital Marketing','Autonomous Systems'];
+    const btnarray=['popular','Machine Learning and Deep Learning','Data Science','Artificial Intelligence','Mobile Development','Web Development','Virtual Reality','Digital Marketing','Autonomous Systems'];
     return (
       <div className="App">
       <div>
-        <h1>Select From Below List Of Courses</h1>
+        <p>Choose From Our Category Of Courses</p>
          {this.renderButtons(btnarray)}
+      </div>
+      <div>
+      <div className="row">{this.renderCards(this.state.course)}</div>
       </div>
       </div>
     );
--- src/Cards.js
+++ src/Cards.js
@@ -1,21 +1,20 @@
 import React,{ Component } from 'react';
 import './App.css';
-import imgsrc from './assets/images/img_avatar.png'
+import imgsrc from './assets/images/img_avatar.png';
 class Cards extends Component {
   render()
   {
     return (
-      <div className="card">
-  <img src={imgsrc} alt="Avatar" style={imgStyle}/>
-  <div className="container">
-    return <h4><b></b></h4>
-    <p>Interior Designer</p>
-  </div>
-</div>
+         <div className="column">
+         <div className="card">
+                <img src={imgsrc} alt="Avatar"/>
+                <div className="container">
+                     <h4>{this.props.name}</h4>
+                      <p>{this.props.instructor}</p>
+                </div>
+         </div>
+        </div>
     );
   }
-}
-var imgStyle={
-  width:'100%'
 }
 export default Cards;
